3d23ad41ac3b165f9ef74640eee9cb771f699109
Rob Prouse <rob@prouse.org>
2018-09-26 22:06:54 -0400

    Updated to take the image from the command line and load the API Key from a file

---
+++ FaceRecognition/Services/TokenReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace FaceRecognition.Services
+{
+    public static class TokenReader
+    {
+        const string TOKEN = "TOKEN";
+
+        public static string GetApiToken()
+        {
+            string token = null;
+            if (File.Exists(TOKEN))
+            {
+                using (var reader = new StreamReader(TOKEN))
+                {
+                    token = reader.ReadToEnd();
+                }
+            }
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                throw new Exception("The file TOKEN must exist with the Azure subscription");
+            }
+            return token;
+        }
+    }
+}
