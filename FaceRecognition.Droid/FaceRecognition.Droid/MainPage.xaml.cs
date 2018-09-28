8137bc26e22f1de19cd6d0ca07793c7460b60269
Rob Prouse <rob@prouse.org>
2018-09-27 22:02:10 -0400

    Step 1 - Xamarin app that takes photos

---
+++ FaceRecognition.Droid/FaceRecognition.Droid/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Xamarin.Forms;
+
+namespace FaceRecognition.Droid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            CameraButton.Clicked += OnCameraButtonClicked;
+        }
+
+        private async void OnCameraButtonClicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                Directory = "Faces",
+                PhotoSize = PhotoSize.Medium
+            });
+
+            if (file == null)
+                return;
+
+            PhotoImage.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                return stream;
+            });
+        }
+    }
+}
